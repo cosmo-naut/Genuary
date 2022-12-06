153261a4a747874aa1eab3fc452431d7f32f78d3
Aidan Tesselaar <aidan.tesla@live.com>
2022-12-06 11:43:14 +1100

    remixed styles a bit

--- gallery/script.js
+++ gallery/script.js
@@ -1,18 +1,19 @@
 import generators from "../generators/generators.js";
+const backgroundColour = 34;
 
-new p5((s) => {
-    s.setup = () => {
-        const canvas = s.createCanvas(512, 512);
+new p5((p5) => {
+    p5.setup = () => {
+        const canvas = p5.createCanvas(512, 512);
         canvas.parent("frame");
 
-        document.getElementById("regenerateButton").addEventListener("click", regenerate);
+        document.getElementsByClassName("roll")[0].addEventListener("click", regenerate);
     }
 
-    s.draw = () => {
-        s.background(34);
-        // generators[day].draw()
+    p5.draw = () => {
+        generators[day].draw(p5)
     }
 });
+
 
 let currentDay = 0;
 let day = 0;
